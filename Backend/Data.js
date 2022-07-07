220ca41e887d2d469c96e9c1b7e5cffb77cdd5ad
dungnn <dungnn1997@gmail.com>
2022-07-07 14:46:38 +0700

    update

---
+++ Backend/Data.js
@@ -0,0 +1,105 @@
+const data ={
+    products:[
+    {    
+        _id : '1',
+        name:'Nike Shirt 1',
+        image:'../images/nike_tshirt_1.jpg',
+        price: 120,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: "High quality product",
+        countInStock : 10
+    },
+    {    
+        _id : '2',
+        name:'Nike Shirt 2',
+        image:'../images/nike_tshirt_2.jpg',
+        price: 220,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 110,
+        description: "The best product of the year",
+        countInStock : 5
+    },
+    {    
+        _id : '3',
+        name:'Nike Shirt 3',
+        image:'../images/nike_tshirt_3.jpg',
+        price: 90,
+        brand: 'Nike',
+        rating: 1.5,
+        numReviews: 30,
+        description: "Not for daily wear",
+        countInStock : 0
+    },
+    {    
+        _id : '4',
+        name:'Nike TShirt 4',
+        image:'../images/nike_tshirt_4.jpg',
+        price: 120,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 3,
+        description: "New product",
+        countInStock : 6
+    },
+    {    
+        _id : '5',
+        name:'Nike Sneaker 1',
+        image:'../images/nike_sneaker_1.jpg',
+        price: 220,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 1,
+        description: "New sneaker",
+        countInStock : 70
+    } ,  
+    {    
+        _id : '6',
+        name:'Nike Sneaker 2',
+        image:'../images/nike_sneaker_2.jpg',
+        price: 120,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 3,
+        description: "New product",
+        countInStock : 0
+    },   
+    {    
+        _id : '7',
+        name:'Nike Sneaker 3',
+        image:'../images/nike_sneaker_3.jpg',
+        price: 120,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 3,
+        description: "New product",
+        countInStock : 100
+    },   
+    {    
+        _id : '8',
+        name:'Nike Sneaker 4',
+        image:'../images/nike_sneaker_4.jpg',
+        price: 120,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 3,
+        description: "New product",
+        countInStock : 90
+    }, 
+    {    
+        _id : '9',
+        name:'Computer Case',
+        image:'../images/computer_01.jpg',
+        price: 1200,
+        brand: 'Dell',
+        rating: 5,
+        numReviews: 10,
+        description: "Dell is headquartered in Round Rock, Texas. The company, first named PC's Limited, was founded in 1984 by American Michael Dell, who was then a student at the University of Texas in Austin. Initially running the business from a dormitory room, Dell started out providing customized upgrades for PCs. ",
+        countInStock : 90
+    }      
+    ]
+}
+
+export default data
---
+++ Backend/Server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import data from "./Data.js"
+const app = express();
+app.get('/',(req,res)=>{
+    res.send('Server is ready at http://localhost:3000');
+});
+app.get('/api/products',(req,res)=>{
+    res.send(data.products)
+})
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server at http://localhost:${port}`)
+})
