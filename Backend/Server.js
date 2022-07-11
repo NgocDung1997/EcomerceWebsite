b24299cb46d9878a1cb7cc5fc778db735d04c4f6
dungnn <dungnn1997@gmail.com>
2022-07-11 10:54:53 +0700

    update redux

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,9 +4,15 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send('Server is ready at http://localhost:3000');
 });
-app.get('/api/products',(req,res)=>{
+app.get('/api/products/',(req,res)=>{
     res.send(data.products)
 })
+
+app.get('/api/products/:id',(req,res)=>{
+    //res.send(data.products)
+    res.send(data.products.find(x => x._id == req.params.id.toString()))
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server at http://localhost:${port}`)
